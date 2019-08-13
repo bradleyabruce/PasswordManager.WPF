cc825c996b0973ba7fec8be4722415b4f38fe7a2
bruceba <bruceba@mail.uc.edu>
2019-08-13 00:19:38 -0400

    began adding logo lookup

---
+++ PasswordManager.WPF/PasswordManager.WPF/DataAccess/DataGetLogo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PasswordManager.WPF.DataAccess
+{
+   class DataGetLogo
+   {
+      DataUtility du = new DataUtility();
+
+
+      public async Task<BitmapImage> GetImage(string domain)
+      {
+         try
+         {
+
+            string url = "https://logo.clearbit.com/" + domain + "?size=200&format=png";
+            Uri uri = du.CreateURI(url);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = "GET";
+
+            BitmapImage image = await Task.Run(() => getResponse(request));
+            return image;
+         }
+         catch
+         {
+            return null;
+         }
+      }
+
+      BitmapImage BitmapToImageSource(Bitmap bitmap)
+      {
+         using (MemoryStream memory = new MemoryStream())
+         {
+            bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+            memory.Position = 0;
+            BitmapImage bitmapimage = new BitmapImage();
+            bitmapimage.BeginInit();
+            bitmapimage.StreamSource = memory;
+            bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapimage.EndInit();
+
+            return bitmapimage;
+         }
+      }
+
+      public BitmapImage getResponse(HttpWebRequest request)
+      {
+         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+         Stream stream = response.GetResponseStream();
+
+         Bitmap bmp = new Bitmap(stream);
+
+         BitmapImage image = BitmapToImageSource(bmp);
+
+         return image;
+      }
+   }
+}
--- PasswordManager.WPF/PasswordManager.WPF/UserControls/PasswordCard.xaml.cs
+++ PasswordManager.WPF/PasswordManager.WPF/UserControls/PasswordCard.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using PasswordManager.WPF.DataAccess;
+using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PasswordManager.WPF.UserControls
 {
@@ -21,6 +15,8 @@ namespace PasswordManager.WPF.UserControls
    public partial class PasswordCard : UserControl
    {
       #region Variables
+      DataGetLogo dgl = new DataGetLogo();
+
 
       private string _domain;
       public string Domain
@@ -37,32 +33,44 @@ namespace PasswordManager.WPF.UserControls
       #endregion Variables
 
 
-      public PasswordCard(string entryid, string domain, string username, string password, string category)
+      public async PasswordCard(string entryid, string domain, string username, string password, string category)
       {
          InitializeComponent();
          Domain = domain;
 
-         //TODO look up website icon
+         Task<BitmapImage> imageasync = dgl.GetImage(domain);
 
-         //otherwise set default icon
-         ImageBox.Visibility = Visibility.Collapsed;
-         IconBox.Visibility = Visibility.Visible;
-         IconBox.Icon = FontAwesome.WPF.FontAwesomeIcon.Key;
+         //loading
+
+         BitmapImage image = await imageasync;
+         if (image != null)
+         {
+            ImageBox.Visibility = Visibility.Visible;
+            IconBox.Visibility = Visibility.Collapsed;
+            ImageBox.Source = image;
+         }
+         else
+         {
+            //otherwise set default icon
+            ImageBox.Visibility = Visibility.Collapsed;
+            IconBox.Visibility = Visibility.Visible;
+            IconBox.Icon = FontAwesome.WPF.FontAwesomeIcon.Key;
+         }
       }
 
       private void Border_MouseEnter(object sender, MouseEventArgs e)
       {
          Border border = sender as Border;
-         border.Background = Brushes.LightBlue;
-         TextBoxDomainName.Background = Brushes.LightBlue;
+         border.Background = System.Windows.Media.Brushes.LightBlue;
+         TextBoxDomainName.Background = System.Windows.Media.Brushes.LightBlue;
          this.Cursor = Cursors.Hand;
       }
 
       private void Border_MouseLeave(object sender, MouseEventArgs e)
       {
          Border border = sender as Border;
-         border.Background = Brushes.White;
-         TextBoxDomainName.Background = Brushes.White;
+         border.Background = System.Windows.Media.Brushes.White;
+         TextBoxDomainName.Background = System.Windows.Media.Brushes.White;
          this.Cursor = Cursors.Arrow;
       }
    }
